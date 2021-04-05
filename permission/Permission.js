1f7b4627bc8f86917462b2eaa699fdc72b9431f6
Rovan Lama <69307463+lamarovan@users.noreply.github.com>
2021-04-05 16:07:02 +0545

    first commit

---
+++ permission/Permission.js
@@ -0,0 +1,13 @@
+function authRole(req, res, next) {
+  const ass = req.body.assignee;
+  const user = req.body.user;
+  const owner = req.body.owner;
+  console.log("user", user.email);
+  console.log("ass:", ass);
+  console.log("owner:", owner);
+  if (owner === user || ass === user || ass === "") {
+    return next();
+  }
+}
+
+module.exports = { authRole };
